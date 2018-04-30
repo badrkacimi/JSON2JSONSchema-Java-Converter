8b39fcf5a0a3085c34fa7e5ef86d951584ba3e63
Badr Kacimi <badrvkacimi@gamail.com>
2018-04-30 16:34:51 +0100

    projet 22

---
+++ src/main/java/com/badrkacimi/JSON2SCHEMA/App.java
@@ -0,0 +1,13 @@
+package com.badrkacimi.JSON2SCHEMA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/badrkacimi/schemagenerator/JacksonJsonSchemaGenerator.java
@@ -0,0 +1,103 @@
+		package com.badrkacimi.schemagenerator;
+		
+		import java.io.BufferedWriter;
+		import java.io.File;
+		import java.io.FileWriter;
+		import java.io.IOException;
+		import java.io.StringWriter;
+		import java.net.URL;
+		import java.util.ArrayList;
+		import java.util.Enumeration;
+		import java.util.List;
+		
+		import com.fasterxml.jackson.core.JsonGenerationException;
+		import com.fasterxml.jackson.databind.JsonMappingException;
+		import com.fasterxml.jackson.databind.ObjectMapper;
+		import com.fasterxml.jackson.databind.SerializationFeature;
+		import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+		import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+		
+		/**
+	     *Hiveprod-Badr Kac
+		 */
+		public class JacksonJsonSchemaGenerator {
+			 public static void main(String[] args)
+					throws ClassNotFoundException {
+		
+				try {
+		            Class[] classes=getClasses("com.hiveprod.pojogen");//input
+			for (Class clazz : classes) {
+		
+				StringWriter jsonSchemafile = generateSchemaMapping(clazz);
+		        createFileWithOption(clazz, jsonSchemafile);
+		}
+		} catch (IOException e) {
+			System.out.println("Probleme rencontre  lors de la conversion en jsonschema: " + e.getMessage());
+				e.printStackTrace();
+			}
+		
+		}
+		
+			 
+		private static StringWriter generateSchemaMapping(Class clazz)
+				throws IOException {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonSchemaGenerator generator = new JsonSchemaGenerator(mapper);
+		
+			JsonSchema jsonSchema = generator.generateSchema(clazz);
+			StringWriter jsonSchemafile = new StringWriter();
+			mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+			//mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
+			mapper.writeValue(jsonSchemafile, jsonSchema);
+			return jsonSchemafile;
+		}
+		
+		private static void createFileWithOption(Class clazz, StringWriter jsonSchemafile) throws IOException {
+			// output
+			File filegeneree = new File("./src/main/resources/generated/" +clazz.getName().replace("java", "").replace("com.hiveprod.pojogen.", "")+".jsonschema");
+			filegeneree.getParentFile().mkdirs();
+			FileWriter fw = new FileWriter(filegeneree.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(jsonSchemafile.toString());
+			System.out.println(filegeneree.getCanonicalPath());
+			bw.close();
+		}		
+		
+		private static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException {
+			
+		    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		    assert classLoader != null;
+		    String path = packageName.replace('.', '/');
+		        Enumeration resources = classLoader.getResources(path);
+		        
+		        List<File> dirs = new ArrayList();
+		        while (resources.hasMoreElements()) {
+		            URL resource = (URL) resources.nextElement();
+		            dirs.add(new File(resource.getFile()));
+		        }
+		        
+		        ArrayList<Class> classes = new ArrayList();
+		        for (File directory : dirs) {
+		            classes.addAll(findClasses(directory, packageName));
+		        }
+		        return classes.toArray(new Class[classes.size()]);
+		    }
+		   
+		    private static List findClasses(File directory, String packageName) throws ClassNotFoundException {
+		        List classes = new ArrayList();
+		        if (!directory.exists()) {
+		            return classes;
+		        }
+		        File[] files = directory.listFiles();
+		        for (File file : files) {
+		            if (file.isDirectory()) {
+		                assert !file.getName().contains(".");
+		classes.addAll(findClasses(file, packageName + "." + file.getName()));
+		} else if (file.getName().endsWith(".class")) {
+		classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+		            }
+		        }
+		        return classes;
+		    }
+		
+		}
